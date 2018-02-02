ed037a7c8112de9fbbc3dc0a874d31e39af19392
Aayush Chaturvedi <aayushchaturvedi1998@gmail.com>
2018-02-03 01:29:32 +0530

    Added Exit-pop up box

--- app/src/main/java/com/example/aac/trial3/MainActivity.java
+++ app/src/main/java/com/example/aac/trial3/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        parentLinearLayout = (LinearLayout) findViewById(R.id.parent_linear_layout);
+        parentLinearLayout = findViewById(R.id.parent_linear_layout);
     }
     public void onAddField(View v) {
         LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -27,4 +27,25 @@ public class MainActivity extends AppCompatActivity {
         parentLinearLayout.removeView((View) v.getParent());
     }
 
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("You are about to exit the App");
+        builder.setMessage("Are You Sure?");
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                finish();
+            }
+        });
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+    
 }
